da47b1290fe54f6a363fc18c6af8bb5a579c5303
Tosin Yadeka <yadekatosin@yahoo.com>
2018-08-04 11:54:19 +0100

    Layout Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,23 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'app';
   constructor(private http: HttpClient){
-     this.http.get("https://www.googleapis.com/youtube/v3/").subscribe();
+  }
+  NoInternet(){
+    this.http.get("https://www.googleapis.com/youtube/v3/").subscribe();
+  }
+  NoPage(){
+    this.http.get("https://www.googleapis.com/youtube/v3/").subscribe();
+  }
+  Error401(){
+    this.http.get("https://www.googleapis.com/youtube/v3/").subscribe();
+  }
+  Error403(){
+    this.http.get("https://www.googleapis.com/youtube/v3/").subscribe();
+  }
+  Error404(){
+    this.http.get("https://www.googleapis.com/youtube/v3/").subscribe();
+  }
+  Error500(){
+    this.http.get("https://xxxasdas").subscribe();
   }
 }
